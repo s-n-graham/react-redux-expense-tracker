dda96eaeafb3dbd3b3884d37cddae91035246158
Simon Graham <sn.graham@outlook.com>
2023-04-05 21:16:06 +0300

    First push

---
+++ src/store/expense/expense-slice.js
@@ -0,0 +1,23 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+export const expenseSlice = createSlice({
+  name: "expenseSlice",
+  initialState: {
+    income: 1000,
+    expenseList: [],
+    countActionPerformed: 0
+  },
+  reducers: {
+    addExpenseAction: (currentSlice, action) => {
+      currentSlice.expenseList.push(action.payload);
+    },
+    setIncomeAction: (currentSlice, action) => {
+      currentSlice.income = action.payload;
+    },
+    incrementActionPerformed: (currentSlice, action) => {
+      currentSlice.countActionPerformed++
+    },
+  },
+});
+
+export const { addExpenseAction, setIncomeAction, incrementActionPerformed } = expenseSlice.actions;
